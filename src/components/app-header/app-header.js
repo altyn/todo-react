16263edec20430a36c7294c9397c137921c39472
Altyn Sh <altynbek.shaidyldaev@gmail.com>
2019-10-03 21:18:21 +0600

    Final release

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import './app-header.css';
+
+const AppHeader = ({toDo, done}) => {
+    return (
+        <div className="app-header d-flex">
+            <h1>Todo List</h1>
+            <h2>{toDo} more or to do, {done} done</h2>
+        </div>
+    );
+};
+
+export default AppHeader;
